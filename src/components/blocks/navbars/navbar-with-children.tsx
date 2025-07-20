0865aad54eda031e155c7506941e16b05f0620b9
Ethan Lo <132605595+ethanol-lo@users.noreply.github.com>
2025-07-19 23:32:12 -0400

    Update navbar-with-children.tsx

--- src/components/blocks/navbars/navbar-with-children.tsx
+++ src/components/blocks/navbars/navbar-with-children.tsx
@@ -215,12 +215,7 @@ const Logo = () => {
 };
 
 const transition = {
-  type: "spring",
-  mass: 0.5,
-  damping: 11.5,
-  stiffness: 100,
-  restDelta: 0.001,
-  restSpeed: 0.001,
+  duration: 0.3,
 };
 
 export const MenuItem = ({
